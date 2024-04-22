ad273dcbbd7ca4c122106311361b7c5499694c81
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-04-23 08:41:05 +1000

    Challenge 1

---
+++ Chapter 2/challengechapter2.py
@@ -0,0 +1,9 @@
+# Write a program with a variable called age assigned to an integer that prints different
+# strings depending on what integer age is.
+
+age = input('Enter your age: ')
+
+if int(age) <= 18:
+    print("You can't drink yet")
+else :
+    print("Okay, you can drink")
