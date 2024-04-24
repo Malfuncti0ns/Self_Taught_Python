b15d308f3e18b28c042fcdf105a7382db91cf24a
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-04-24 16:26:06 +1000

    Challenge 5

---
+++ Chapter5Challenge.py
@@ -0,0 +1,32 @@
+# Challenges
+# 1. Create a list of your favorite musicians.
+
+fav_musicians = ['In Hearts Wake', 'Four Year Strong', 'Enter Shikari', 'Mom Jeans', 'Hot Mulligan']
+
+print(fav_musicians)
+
+# 2. Create a list of tuples, with each tuple containing the longitude and latitude of somewhere you’ve lived or visited.
+
+visitor = (['Croatia', '45.1000 N', '15.2000 E'], ['Italy', '41.8719', '12.5674'], ['Amsterdam', '52.3676', '4.9041'])
+
+print(visitor)
+print(visitor[2])
+
+# 3. Create a dictionary that contains different attributes about you: height, favorite color, favorite author, etc.
+
+details = {'height' : 173,
+           'Colour' : 'Red',
+           'Author' : 'J.R.R Tolkien'
+           }
+
+print(details)
+
+# 4. Write a program that lets the user ask your height, favorite color, or favorite author, and returns the result from the dictionary 
+# you created in the previous challenge.
+
+
+
+
+
+# 5. Create a dictionary mapping your favorite musicians to a list of your favorite songs by them.
+# 6. Lists, tuples, and dictionaries are just a few of the containers built into Python. Research Python sets (a type of container). When would you use a set?
