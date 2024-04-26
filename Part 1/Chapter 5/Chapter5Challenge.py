20d3ea732a342bfb96d85c4b1c2c0c463007241a
Malfuncti0ns <matthewchiandotto1@gmail.com>
2024-04-26 10:01:06 +1000

    Updated structure, and questions answered

--- Part 1/Chapter 5/Chapter5Challenge.py
+++ Part 1/Chapter 5/Chapter5Challenge.py
@@ -12,6 +12,10 @@ visitor = (['Croatia', '45.1000 N', '15.2000 E'], ['Italy', '41.8719', '12.5674'
 print(visitor)
 print(visitor[2])
 
+# Did not make a list of tuples, I made a tuple of lists
+
+visitor_fixed = [('Croatia', '45.1000 N', '15.2000 E'), ('Italy', '41.8719', '12.5674'), ('Amsterdam', '52.3676', '4.9041')]
+
 # 3. Create a dictionary that contains different attributes about you: height, favorite color, favorite author, etc.
 
 details = {'height' : 173,
@@ -33,13 +37,28 @@ details2 = {
 print(details2)
 print(details2['Author'])
 
+# Anser for 4 below with their example dict, should have looped the query from my own dict instead of prompting and adding
+# Need to READ the question, details were missed
+
+me = {
+    "height": "6",
+    "fav_color": "red",
+    "fav_author": "Orwell"
+}
+
+answer = input("Type height, fav_color or fav_author")
+if answer in me:
+    result = me[answer]
+    print(result)
+
+
 # 5. Create a dictionary mapping your favorite musicians to a list of your favorite songs by them.
 artists = {
     'metal' : ['Sleep Token', 'Parkway Drive','Ghost'],
     'rap' : ['Kendrick Lamar', 'Mac Miller', 'Amine'],
     'dnb' : ['Chase and Status', 'Dimension', '1991']
 }
-print(artists['rap'])
+print(artists)
 
 # 6. Lists, tuples, and dictionaries are just a few of the containers built into Python. Research Python sets (a type of container). When would you use a set?
 
